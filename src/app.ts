cc726edac0f441103cdb1d4e9c7b53f02ce6ef52
jacleklm <1061161721@qq.com>
2022-10-03 20:09:51 +0800

    feat: init

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,44 @@ import './app.scss'
 import {
   Button,
   Toast,
+  Avatar,
+  Icon,
+  Popup,
+  OverLay,
+  TextArea,
+  Tabs,
+  TabPane,
   Tabbar,
   TabbarItem,
-  Icon
+  InputNumber,
+  ShortPassword,
+  SearchBar,
+  Checkbox,
+  CheckboxGroup
 } from '@nutui/nutui-taro';
 import store from './store/index'
 
 const App = createApp({
   onShow(options) { },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-}).use(store).use(Button).use(Toast).use(Tabbar).use(TabbarItem).use(Icon)
+})
+  .use(store)
+  .use(Button)
+  .use(Toast)
+  .use(Icon)
+  .use(Avatar)
+  .use(Popup)
+  .use(OverLay)
+  .use(TextArea)
+  .use(Tabs)
+  .use(TabPane)
+  .use(Tabbar)
+  .use(TabbarItem)
+  .use(InputNumber)
+  .use(ShortPassword)
+  .use(Popup)
+  .use(SearchBar)
+  .use(CheckboxGroup)
+  .use(Checkbox)
 
 export default App
