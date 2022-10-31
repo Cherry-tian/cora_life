d3e759f443f3771f36ca7dec2fee552f12c05cfa
jacleklm <1061161721@qq.com>
2022-10-31 16:46:03 +0800

    feat: 上线版;提交审核!

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,66 @@
 import { createApp } from 'vue'
 import './app.scss'
-import NutUI from "@nutui/nutui-taro"
-import "@nutui/nutui-taro/dist/style.css";
 import store from './store/index'
 
+// taro 全局加载
+// import NutUI from "@nutui/nutui-taro"
+// import "@nutui/nutui-taro/dist/style.css";
+// const App = createApp({
+//   onShow(options) { },
+//   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+// }).use(NutUI).use(store)
+
+// taro 按需加载
+import {
+  Button,
+  Avatar,
+  Icon,
+  Popup,
+  OverLay,
+  TextArea,
+  Tabs,
+  TabPane,
+  Tabbar,
+  TabbarItem,
+  ImagePreview,
+  Input,
+  Radio,
+  RadioGroup,
+  Cell,
+  CellGroup,
+  Uploader,
+  Progress,
+  Swiper,
+  SwiperItem,
+  InfiniteLoading,
+  Empty,
+} from "@nutui/nutui-taro";
+
 const App = createApp({
-  onShow(options) { },
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-}).use(NutUI).use(store)
+  onShow(options) {},
+})
+  .use(store)
+  .use(Button)
+  .use(Icon)
+  .use(Avatar)
+  .use(OverLay)
+  .use(TextArea)
+  .use(Tabs)
+  .use(TabPane)
+  .use(Tabbar)
+  .use(TabbarItem)
+  .use(Popup)
+  .use(ImagePreview)
+  .use(Input)
+  .use(Radio)
+  .use(RadioGroup)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Uploader)
+  .use(Progress)
+  .use(Swiper)
+  .use(SwiperItem)
+  .use(InfiniteLoading)
+  .use(Empty)
 
 export default App
